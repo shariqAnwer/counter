5ac574ac5623fc51926675ade8ce781807ce8c55
Md Sharique Anwer <shariqueanwer002@gmail.com>
2022-04-14 03:19:24 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {Counter} from './components/Counter'
+import Counter from './components/Counter'
 
 function App() {
   return (
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,37 +1,25 @@
-import { useState } from "react";
-import './Counter.css'
+import React from "react";
 
-export function Counter() {
-    let [counter, setCounter] = useState(0);
-    const handleChange = (value) => {
-        setCounter(counter + value);
-    }
-   return (
-       <div class="Counter">
-           <h1 class= "bigFont">Counter : {counter}</h1>
-           <button
-            onClick={() => {
-                handleChange(1)
-                }
-            }>
-                Increment
-            </button>
-            <button
-            onClick={() => {
-                if(counter >= 1) {
-                    handleChange(-1)
-                }
-                }
-            }>
-                Decrement
-            </button>
-           <button
-            onClick={() => {
-                handleChange(Math.pow(counter, 1))
-                }
-            }>
-                Double
-            </button>
-       </div>
-   ) 
+export default function Counter() {
+  const [counter, setCounter] = React.useState(1); //hooks
+  //destructuring
+  //React.useState => [value, setvalue]
+  const handleIncrement = (value) => {
+    setCounter(counter + value);
+  };
+
+  const handleDouble = (value) => {
+    setCounter(counter * value);
+  };
+
+  return (
+    <>
+      <h1>Counter</h1>
+
+      <h2 style={{ color: counter % 2 === 0 ? "green" : "red" }}>{counter}</h2>             
+      <button onClick={() => handleIncrement(1)}>Add</button>
+      <button onClick={() => handleIncrement(-1)}>Reduce</button>
+      <button onClick={() => handleDouble(2)}>Double</button>
+    </>
+  );
 }
